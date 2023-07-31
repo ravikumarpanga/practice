71675093848a42a7cdea1af9d2de0c9450a1647c
ravi-kumar-s-code <ravikumar.marolix@gmail.com>
2023-07-31 17:25:05 +0530

    practice file

---
+++ practice.py
@@ -0,0 +1,16 @@
+# Take two numbers check which one is greater among them
+
+"""r=int(input("Enter a number of r:"))
+s=int(input("Enter a number of s:"))
+if r>s:
+    print("r is greater then s")
+else:
+    print("s is greater then r")"""
+
+
+r=int(input("Enter a number of r:"))
+s=int(input("Enter a number of s:"))
+if r>s:
+    print("r is greater then s")
+elif r<s:
+    print("s is greater then r")
