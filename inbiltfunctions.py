df5d6b8a24376a37849d5621684556740a953135
ravikumarpanga <140778297+ravikumarpanga@users.noreply.github.com>
2023-09-11 20:28:36 +0530

    Add files via upload

---
+++ inbiltfunctions.py
@@ -0,0 +1,294 @@
+#string
+#------------------------
+
+# Built-In Function
+
+# Description
+
+# string.ascii_letters	
+# Concatenation of the ascii_lowercase and ascii_uppercase constants.
+
+# string.ascii_lowercase	
+# Concatenation of lowercase letters
+
+# string.ascii_uppercase	
+# Concatenation of uppercase letters
+
+# string.digits	
+# Digit in strings
+
+# string.hexdigits	
+# Hexadigit in strings
+
+# string.letters
+
+# concatenation of the strings lowercase and uppercase
+
+# string.lowercase
+
+# A string must contain lowercase letters.
+
+# string.octdigits
+
+# Octadigit in a string
+
+# string.punctuation
+
+# ASCII characters having punctuation characters.
+
+# string.printable
+
+# String of characters which are printable
+
+# String.endswith()	
+# Returns True if a string ends with the given suffix otherwise returns False
+
+# String.startswith()	
+# Returns True if a string starts with the given prefix otherwise returns False
+
+# String.isdigit()	
+# Returns “True” if all characters in the string are digits, Otherwise, It returns “False”.
+
+# String.isalpha()	
+# Returns “True” if all characters in the string are alphabets, Otherwise, It returns “False”.
+
+# string.isdecimal()	
+# Returns true if all characters in a string are decimal.
+
+# str.format()	
+# one of the string formatting methods in Python3, which allows multiple substitutions and value formatting.
+
+# String.index	
+# Returns the position of the first occurrence of substring in a string
+
+# string.uppercase
+
+# A string must contain uppercase letters.
+
+# string.whitespace	
+# A string containing all characters that are considered whitespace.
+
+# string.swapcase()	
+# Method converts all uppercase characters to lowercase and vice versa of the given string, and returns it
+
+# replace()	
+# returns a copy of the string where all occurrences of a substring is replaced with another substring.
+
+# Built-In Function
+
+# Description
+
+# string.Isdecimal	
+# Returns true if all characters in a string are decimal
+
+# String.Isalnum	
+# Returns true if all the characters in a given string are alphanumeric.
+
+# string.Istitle	
+# Returns True if the string is a title cased string
+
+# String.partition	
+# splits the string at the first occurrence of the separator and returns a tuple.
+
+# String.Isidentifier	
+# Check whether a string is a valid identifier or not.
+
+# String.len	
+# Returns the length of the string.
+
+# String.rindex	
+# Returns the highest index of the substring inside the string if substring is found.
+
+# String.Max	
+# Returns the highest alphabetical character in a string.
+
+# String.min	
+# Returns the minimum alphabetical character in a string.
+
+# String.splitlines	
+# Returns a list of lines in the string.
+
+# string.capitalize	
+# Return a word with its first character capitalized.
+
+# string.expandtabs	
+# Expand tabs in a string replacing them by one or more spaces
+
+# string.find	
+# Return the lowest indexing a sub string.
+
+# string.rfind	
+# find the highest index.
+
+# string.count	
+# Return the number of (non-overlapping) occurrences of substring sub in string
+
+# string.lower	
+# Return a copy of s, but with upper case, letters converted to lower case.
+
+# string.split	
+# Return a list of the words of the string, If the optional second argument sep is absent or None
+
+# string.rsplit()	
+# Return a list of the words of the string s, scanning s from the end.
+
+# rpartition()	
+# Method splits the given string into three parts
+
+# string.splitfields
+
+# Return a list of the words of the string when only used with two arguments.
+
+# string.join	
+# Concatenate a list or tuple of words with intervening occurrences of sep.
+
+# string.strip()	
+# It returns a copy of the string with both leading and trailing white spaces removed
+
+# string.lstrip	
+# Return a copy of the string with leading white spaces removed.
+
+# string.rstrip	
+# Return a copy of the string with trailing white spaces removed.
+
+# string.swapcase	
+# Converts lower case letters to upper case and vice versa.
+
+# string.translate	
+# Translate the characters using table
+
+# string.upper	
+# lower case letters converted to upper case.
+
+# string.ljust	
+# left-justify in a field of given width.
+
+# string.rjust	
+# Right-justify in a field of given width.
+
+# string.center()	
+# Center-justify in a field of given width.
+
+# string-zfill	
+# Pad a numeric string on the left with zero digits until the given width is reached.
+
+# string.replace	
+# Return a copy of string s with all occurrences of substring old replaced by new.
+
+# string.casefold()	
+# Returns the string in lowercase which can be used for caseless comparisons.
+
+# string.encode	
+# Encodes the string into any encoding supported by Python. The default encoding is utf-8.
+
+# string.maketrans	
+# Returns a translation table usable for str.translate()
+
+#list
+#------------------
+
+# Function	Description
+# Append()	Add an element to the end of the list
+# Extend()	Add all elements of a list to another list
+# Insert()	Insert an item at the defined index
+# Remove()	Removes an item from the list
+# Clear()	Removes all items from the list
+# Index()	Returns the index of the first matched item
+# Count()	Returns the count of the number of items passed as an argument
+# Sort()	Sort items in a list in ascending order
+# Reverse()	Reverse the order of items in the list
+# copy()	Returns a copy of the list
+# pop()	Removes and returns the item at the specified index. If no index is provided, it removes and returns the last item.
+
+# Function	Description
+# reduce()	apply a particular function passed in its argument to all of the list elements stores the intermediate result and only returns the final summation value
+# sum()	Sums up the numbers in the list
+# ord()	Returns an integer representing the Unicode code point of the given Unicode character
+# cmp()	This function returns 1 if the first list is “greater” than the second list
+# max()	return maximum element of a given list
+# min()	return minimum element of a given list
+# all()	Returns true if all element is true or if the list is empty
+# any()	return true if any element of the list is true. if the list is empty, return false
+# len()	Returns length of the list or size of the list
+# enumerate()	Returns enumerate object of the list
+# accumulate()	apply a particular function passed in its argument to all of the list elements returns a list containing the intermediate results
+# filter()	tests if each element of a list is true or not
+# map()	returns a list of the results after applying the given function to each item of a given iterable
+# lambda()	This function can have any number of arguments but only one expression, which is evaluated and returned.
+
+#Set Methods
+#--------------------------
+
+# Function	Description
+# add()	Adds an element to a set
+# remove()	Removes an element from a set. If the element is not present in the set, raise a KeyError
+# clear()	Removes all elements form a set
+# copy()	Returns a shallow copy of a set
+# pop()	Removes and returns an arbitrary set element. Raise KeyError if the set is empty
+# update()	Updates a set with the union of itself and others
+# union()	Returns the union of sets in a new set
+# difference()	Returns the difference of two or more sets as a new set
+# difference_update()	Removes all elements of another set from this set
+# discard()	Removes an element from set if it is a member. (Do nothing if the element is not in set)
+# intersection()	Returns the intersection of two sets as a new set
+# intersection_update()	Updates the set with the intersection of itself and another
+# isdisjoint()	Returns True if two sets have a null intersection
+# issubset()	Returns True if another set contains this set
+# issuperset()	Returns True if this set contains another set
+# symmetric_difference()	Returns the symmetric difference of two sets as a new set
+# symmetric_difference_update()	Updates a set with the symmetric difference of itself and another
+
+#tuple
+#-----------------
+
+# Built-in-Method	Description
+# index( )	Find in the tuple and returns the index of the given value where it’s available
+# count( )	Returns the frequency of occurrence of a specified value
+# Built-In Functions
+# Built-in Function	Description
+# all()	Returns true if all element are true or if tuple is empty
+# any()	return true if any element of the tuple is true. if tuple is empty, return false
+# len()	Returns length of the tuple or size of the tuple
+# enumerate()	Returns enumerate object of tuple
+# max()	return maximum element of given tuple
+# min()	return minimum element of given tuple
+# sum()	Sums up the numbers in the tuple
+# sorted()	input elements in the tuple and return a new sorted list
+# tuple()	Convert an iterable to a tuple.
+
+ #dict
+#--------------------------
+# # demo for all dictionary methods
+# dict1 = {1: "Python", 2: "Java", 3: "Ruby", 4: "Scala"}
+  
+# # copy() method
+# dict2 = dict1.copy()
+# print(dict2)
+  
+# # clear() method
+# dict1.clear()
+# print(dict1)
+  
+# # get() method
+# print(dict2.get(1))
+  
+# # items() method
+# print(dict2.items())
+  
+# # keys() method
+# print(dict2.keys())
+  
+# # pop() method
+# dict2.pop(4)
+# print(dict2)
+  
+# # popitem() method
+# dict2.popitem()
+# print(dict2)
+  
+# # update() method
+# dict2.update({3: "Scala"})
+# print(dict2)
+  
+# # values() method
+# print(dict2.values())
